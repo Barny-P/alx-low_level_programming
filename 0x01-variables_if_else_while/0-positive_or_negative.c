3e53298f4b91534be2004bbccd4345eb9b98fa7c
Barny-P <petuonovbe@gmail.com>
2022-07-01 17:50:04 -0700

    Print alphabet

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,36 +1,32 @@
 #include <stdlib.h>
-
 #include <time.h>
+#include <stdio.h>
 
-/* more headers goes there */
-
-
-
-/* betty style doc for function main goes there */
+/**
+ *main - assign a random number to the variable n each time it is executed
+ *and prints out based on condition
+ *
+ *Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
 
-	int n;
+        int n;
 
-
-	srand(time(0));
-
-	n = rand() - RAND_MAX / 2;
-
-	/* your code goes there */
-
-	if(n > 0) {
-	      	printf("is positive \n");
+        srand(time(0));
+ 	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+	printf("%d is %s\n", n, "positive");					   }
+	else if (n < 0)
+	{	
+	printf("%d is %s\n", n, "negative");
+	}									
+   	else	
+	{
+	printf("%d is %s\n", n, "zero");
 	}
-	if(n == 0) {
-		printf("is zero \n");
-	}
-	if(n < 0) {
-		printf("is negative \n");
-	}
-
-	return (0);
-
+  	return (0);
 }

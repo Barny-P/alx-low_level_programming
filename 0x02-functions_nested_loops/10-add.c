4357b843c5f8bedb3482603fb0e60d8a0b37563c
Barny-P <petuonovbe@gmail.com>
2022-07-06 16:06:22 -0700

    Hot fix

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * int add - adds two integer and returns the result
+ * Return: Always 0
+ *
+ */
+int add(int, int);
+{
+	int n;
+	n = add(89, 9);
+	printf("%d\n", n));
+	return (0);
+}

1b7e5156f41499f20a590366a671935ac345d16a
Barny-P <petuonovbe@gmail.com>
2022-07-05 19:38:11 -0700

    Print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabets in lowercase x10
+ */
+void print_alphabet_x10(void)
+{
+	int p;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (p = 'a'; p <= 'z'; p++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
